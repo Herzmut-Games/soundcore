7ad98668f32719e21fc72159c0a8b42cfba66c90
Joshua Grimm <joshua.grimm@hetzner-cloud.de>
2019-11-21 18:52:57 +0100

    play custom sounds, don't play sounds if spotify is running

--- core.py
+++ core.py
@@ -6,7 +6,7 @@ import pygame
 import random
 import alsaaudio
 
-sounddir            = "/home/pi/soundcore/sounds"
+sounddir            = "/home/pi/git/soundcore/sounds"
 goalsounddir        = sounddir+"/goal"
 firstgoalsounddir   = sounddir+"/firstgoal"
 startsounddir       = sounddir+"/start"
@@ -19,6 +19,7 @@ ENDSOUNDS          = glob.glob(endsounddir+"/*.mp3")
 
 score_red = 0
 score_white = 0
+spotify_status = "stop"
 
 def on_connect(client, userdata, flags, rc):
     print("Connected to mqtt with result code " + str(rc))
@@ -26,17 +27,20 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe("score/red")
     client.subscribe("game/end")
     client.subscribe("sound/volume")
+    client.subscribe("sound/play")
+    client.subscribe("spotify/status")
 
 def on_message(client, userdata, msg):
     #print("message on "+str(msg.topic)+" payload "+str(msg.payload.decode('utf-8')))
-    global score_red, score_white
+    global score_red, score_white, spotify_status
     try:
+        # volume
         if (str(msg.topic) == "sound/volume"):
             m = alsaaudio.Mixer('PCM')
             print("set volume to "+str(msg.payload.decode('utf-8')))
             m.setvolume(int(msg.payload.decode('utf-8')))
             return
-
+        # set score
         if (str(msg.topic) == "score/red"):
             if (str(msg.payload.decode('utf-8')) == str(score_red)):
                 print("red is already "+str(score_red))
@@ -54,7 +58,6 @@ def on_message(client, userdata, msg):
 
         # return if reset
         if (str(msg.topic) == "score/red" and int(score_red) == 0):
-            print("bla")
             return
         if (str(msg.topic) == "score/white" and int(score_white) == 0):
             return
@@ -66,15 +69,35 @@ def on_message(client, userdata, msg):
         if (str(msg.topic) == "game/end"):
             play_random_sound(ENDSOUNDS)
             return
-        #goal sound
 
-        play_random_sound(GOALSOUNDS)
+        #custom play
+        if (str(msg.topic) == "sound/play"):
+            if str(msg.payload.decode('utf-8')) == "goal":
+                play_random_sound(GOALSOUNDS)
+            elif str(msg.payload.decode('utf-8')) == "end":
+                play_random_sound(ENDSOUNDS)
+            elif str(msg.payload.decode('utf-8')) == "firstgoal":
+                play_random_sound(FIRSTGOALSOUNDS)
+            elif str(msg.payload.decode('utf-8')) == "start":
+                play_random_sound(STARTSOUNDS)
+            return
+        #goal sound
+        if (str(msg.topic) == "score/white" or str(msg.topic) == "score/red"):
+            play_random_sound(GOALSOUNDS)
+
+        # spotify status
+        if (str(msg.topic) == "spotify/status"):
+            spotify_status = str(msg.payload)
 
 
     except Exception as e:
         print(e)
 
 def play_random_sound(list):
+    global spotify_status
+    if spotify_status != stop:
+        print("not playing sound because spotify is playing")
+        return
     sound = random.choice(list)
     print("playing sound "+sound)
     pygame.mixer.music.load(sound)
@@ -86,6 +109,5 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 client.connect("172.30.1.32", 1883, 60)
-
 
 client.loop_forever()
